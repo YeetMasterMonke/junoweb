289c929f06453c4e2fe0d0d9b7200da1f77c98fa
JunoDoesThings <93882967+YeetMasterMonke@users.noreply.github.com>
2026-01-10 18:46:49 -0500

    blehefji
    ergsdrgdg

--- blog-edit.bat
+++ blog-edit.bat
@@ -3,5 +3,8 @@ cd /d "%~dp0"
 echo Starting Blog Server...
 echo Make sure Flask is installed: pip install flask
 echo.
+echo Opening blog editor in browser...
+timeout /t 2 /nobreak
+start http://127.0.0.1:5000/blog-admin.html
 python server.py
 pause
